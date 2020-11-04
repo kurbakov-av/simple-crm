57c9a780f8c94b0a238b3b91ec417d97012055ea
leks <kurbakov.av@gmail.com>
2020-11-04 12:11:29 +0300

    add: SmsMessageDTO

---
+++ src/main/java/com/example/crm/service/sms/SmsMessageDTO.java
@@ -0,0 +1,20 @@
+package com.example.crm.service.sms;
+
+import com.example.crm.domain.sms.SmsMessageStatus;
+import com.example.crm.domain.sms.SmsProviderType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class SmsMessageDTO {
+    private String uuid;
+    private String sender;
+    private String text;
+    private String phone;
+    private SmsMessageStatus status;
+    private LocalDateTime deliveredAt;
+    private String operator;
+    private String price;
+    private SmsProviderType providerType;
+}
