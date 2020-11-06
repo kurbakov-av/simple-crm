baffa2760800d74a2022c4f24ca1521370b97adf
leks <kurbakov.av@gmail.com>
2020-11-06 03:17:03 +0300

    add: SmsSender

--- src/main/java/com/example/crm/domain/sms/provider/SmsProvider.java
+++ src/main/java/com/example/crm/domain/sms/provider/SmsProvider.java
@@ -1,17 +1,34 @@
 package com.example.crm.domain.sms.provider;
 
 import com.example.crm.domain.Catalog;
+import com.example.crm.domain.sms.SmsProviderType;
+import com.example.crm.domain.sms.SmsSender;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 @Table(name = "sms-providers")
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class SmsProvider extends Catalog {
+
+    @NotNull
     private String login;
+
+    @NotNull
     private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private SmsProviderType providerType;
+
+    @ManyToOne(optional = false)
+    private SmsSender defaultSender;
+
+    @OneToMany(mappedBy = "provider", orphanRemoval = true)
+    private List<SmsSender> senders;
 }
