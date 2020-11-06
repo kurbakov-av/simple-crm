f62a72fbc00f6a47081ebf87440d5887a0d627a3
leks <kurbakov.av@gmail.com>
2020-11-06 03:18:22 +0300

    add: find by name sms operator in SmsOperatorRepository

--- src/main/java/com/example/crm/domain/sms/SmsOperatorRepository.java
+++ src/main/java/com/example/crm/domain/sms/SmsOperatorRepository.java
@@ -3,6 +3,9 @@ package com.example.crm.domain.sms;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.Optional;
+
 @RestResource(path = "sms-operators")
 public interface SmsOperatorRepository extends JpaRepository<SmsOperator, Long> {
+    Optional<SmsOperator> findByName(String name);
 }
