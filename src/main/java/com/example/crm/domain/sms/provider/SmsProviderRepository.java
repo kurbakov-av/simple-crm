951f9f8714c0c931d5c75e6b103714ed4dba755b
leks <kurbakov.av@gmail.com>
2020-11-06 03:17:39 +0300

    add: SmsProviderRepository

--- src/main/java/com/example/crm/domain/sms/provider/SmsProviderRepository.java
+++ src/main/java/com/example/crm/domain/sms/provider/SmsProviderRepository.java
@@ -1,9 +1,14 @@
 package com.example.crm.domain.sms.provider;
 
+import com.example.crm.domain.sms.SmsProviderType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Optional;
 
 @RestResource(path = "sms-providers")
 public interface SmsProviderRepository extends JpaRepository<SmsProvider, Long> {
     SmsProvider findByName(String name);
+
+    Optional<SmsProvider> findByProviderType(SmsProviderType type);
 }
